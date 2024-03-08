e64f4488fdd562d61c42b110280a13c5ce9613a6
marinus-g <marinusmail@protonmail.com>
2024-03-08 14:24:08 +0100

    added spring starter files

---
+++ src/test/java/academy/mischok/learningjournal/LearningJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mischok.learningjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
