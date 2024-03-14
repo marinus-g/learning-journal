d062f21db692d956e4f6f12969023142abdd7a47
Firat <firat.polat@tn.mischok.academy>
2024-03-14 10:44:37 +0100

    Repositorys revised

--- src/main/java/academy/mischok/learningjournal/repository/RandomLightningTopicRepository.java
+++ src/main/java/academy/mischok/learningjournal/repository/RandomLightningTopicRepository.java
@@ -1,4 +1,10 @@
 package academy.mischok.learningjournal.repository;
 
-public interface RandomLightningTopicRepository {
+import academy.mischok.learningjournal.model.UserEntity;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RandomLightningTopicRepository extends JpaRepository<UserEntity, Long> {
 }
