aa5e21748c90458c7cf7c72791136741aee1dc64
marinus-g <marinusmail@protonmail.com>
2024-03-15 20:24:52 +0100

    optimized imports

--- src/main/java/academy/mischok/learningjournal/config/SecurityConfig.java
+++ src/main/java/academy/mischok/learningjournal/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package academy.mischok.learningjournal.config;
 
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
