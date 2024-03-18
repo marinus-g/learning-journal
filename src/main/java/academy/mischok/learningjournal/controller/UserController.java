b7d4dfe623515b3753af03417aa998524a0f513d
marinus-g <marinusmail@protonmail.com>
2024-03-18 11:53:42 +0100

    added basic self user edit

--- src/main/java/academy/mischok/learningjournal/controller/UserController.java
+++ src/main/java/academy/mischok/learningjournal/controller/UserController.java
@@ -2,11 +2,12 @@ package academy.mischok.learningjournal.controller;
 
 import academy.mischok.learningjournal.dto.UserDto;
 import academy.mischok.learningjournal.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
