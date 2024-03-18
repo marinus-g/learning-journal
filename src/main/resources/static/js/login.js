455b637bd62a1c15e5f121927efd7b94095d0a0b
marinus-g <marinusmail@protonmail.com>
2024-03-18 09:09:23 +0100

    added user logout

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,16 +1,14 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    function openPasswordReset() {
-        fetch('/login/password-reset')
-            .then(response => response.text())
-            .then(html => {
-                console.log(document.getElementById('passwordResetContainer'))
-                document.getElementById('passwordResetContainer').innerHTML = html;
-                $('.modal').modal('show');
-            });
-    }
-    document.querySelector('.password-reset').addEventListener(
-        'click', openPasswordReset);
-});
+function openPasswordReset() {
+    fetch('/login/password-reset')
+        .then(response => response.text())
+        .then(html => {
+            console.log(document.getElementById('passwordResetContainer'))
+            document.getElementById('passwordResetContainer').innerHTML = html;
+            $('.modal').modal('show');
+        });
+}
+document.querySelector('.password-reset').addEventListener(
+    'click', openPasswordReset);
 // Replace the URL with the current URL without the query string`s
 let url = new URL(window.location.href);
 url.search = '';
