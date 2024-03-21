35cea3f2cd70b4a742112052e12db65feebfa861
Firat <firat.polat@tn.mischok.academy>
2024-03-21 08:36:12 +0100

    Klassenverwaltung configuration

--- src/main/java/academy/mischok/learningjournal/controller/advisor/ClassController.java
+++ src/main/java/academy/mischok/learningjournal/controller/advisor/ClassController.java
@@ -39,4 +39,9 @@ public class ClassController {
 
     }
 
+    @GetMapping("/classrooms/{id}")
+    public String classroom(@PathVariable Long id, Model model){
+        return "Klasse";
+    }
+
     }
